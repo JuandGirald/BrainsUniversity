4cfe574a006b88f68a2622d7c135b3ff07120586
Juan David Giraldo Rodriguez <jdgiraldo88@hotmail.com>
2014-05-16 09:05:40 -0500

    delete admin option in form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -87,7 +87,7 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation, :admin)
+                                   :password_confirmation)
     end
 
 
